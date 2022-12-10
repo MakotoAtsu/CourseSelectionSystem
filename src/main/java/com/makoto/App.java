0f66f995bacd38236362459224aa05335165ffcb
MakotoAtsu <chris03152008@gmail.com>
2022-12-10 10:05:12 +0800

    enable swagger ui

--- src/main/java/com/makoto/App.java
+++ src/main/java/com/makoto/App.java
@@ -1,6 +1,10 @@
 package com.makoto;
 
 import io.javalin.Javalin;
+import io.javalin.openapi.plugin.OpenApiConfiguration;
+import io.javalin.openapi.plugin.OpenApiPlugin;
+import io.javalin.openapi.plugin.swagger.SwaggerConfiguration;
+import io.javalin.openapi.plugin.swagger.SwaggerPlugin;
 
 /**
  * Hello world!
@@ -8,8 +12,13 @@ import io.javalin.Javalin;
  */
 public class App {
     public static void main(String[] args) {
-        Javalin.create()
-                .get("/", ctx -> ctx.result("OK"))
+        Javalin.create(config -> {
+            var openApiConfiguration = new OpenApiConfiguration();
+            openApiConfiguration.getInfo().setTitle("Javalin OpenAPI example");
+            config.plugins.register(new OpenApiPlugin(openApiConfiguration));
+            config.plugins.register(new SwaggerPlugin(new SwaggerConfiguration()));
+        })
+                .get("/", ctx -> ctx.redirect("/swagger")) // redirect to swagger-ui
                 .start(8000);
     }
 }
